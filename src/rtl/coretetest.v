e4e685e7dc20f94c85da8750975fc68cc019c577
Joachim Strömbergson <joachim@secworks.se>
2014-02-10 18:35:44 +0100

    Updating the coretest module interface to what we now expect it to be.

--- src/rtl/coretetest.v
+++ src/rtl/coretetest.v
@@ -1,65 +1,80 @@
-p//======================================================================
+//======================================================================
 //
-// ctttest.v
-// ---------
+// coretest.v
+// ----------
 // Top level of the Cryptech test core. Simply a 32-bit interface
 // with some intrernal functionality to see that we can read and write
 // registers in the FPGA from the host. This core will be the basis
 // for all core top levels.
 //
-// This should be a WISBONE interface.
 //
-// (c) 2014 JoachimS
+// Author: Joachim Strombergson
+// Copyright (c) 2013  Secworks Sweden AB
+// 
+// Redistribution and use in source and binary forms, with or 
+// without modification, are permitted provided that the following 
+// conditions are met: 
+// 
+// 1. Redistributions of source code must retain the above copyright 
+//    notice, this list of conditions and the following disclaimer. 
+// 
+// 2. Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in 
+//    the documentation and/or other materials provided with the 
+//    distribution. 
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS 
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE 
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 //======================================================================
 
-module cttest(
-              input wire           clk,
-              input wire           reset_n,
+module coretest(
+                input wire           clk,
+                input wire           reset_n,
+                
+                // Interface to communication core
+                input wire           rx_syn,
+                input wire [7 : 0]   rx_data,
+                output wire          rx_ack,
+                
+                output wire          tx_syn,
+                output wire [7 : 0]  tx_data,
+                input wire           tx_ack,
+                
+                // Interface to the core being tested.
+                output               core_reset_n,
+                output wire          core_cs,
+                output wire          core_we,
+                output wire [15 : 0] core_address,
+                output wire [31 : 0] core_write_data,
+                input wire  [31 : 0] core_read_data,
+                input wire [31 : 0]  core_error
+               );
 
-              input wire           cs,
-              input wire           we,
-              input wire [2 : 0]   address,
-              input wire [31 : 0]  write_data,
-              output wire [31 : 0] read_data,
-              output wire [31 : 0] api_error
-             );
-
-  
   
   //----------------------------------------------------------------
   // Internal constant and parameter definitions.
   //----------------------------------------------------------------
-  parameter ADDR0 = 3'h0; // Version
-  parameter ADDR1 = 3'h1; // Op A
-  parameter ADDR2 = 3'h2; // Op B
-  parameter ADDR3 = 3'h3; // Result
-
-  // String: ctt1
-  parameter VERSION = 32'h63747431;
 
   
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [31 : 0] testop_a_reg;
-  reg [31 : 0] testop_a_new;
-  reg          testop_a_we;
-  
-  reg [31 : 0] testop_b_reg;
-  reg [31 : 0] testop_b_new;
-  reg          testop_b_we;
-
-  reg [31 : 0] testresult_reg;
-  reg [31 : 0] testresult_new;
-  reg          testresult_we;
 
   
   //----------------------------------------------------------------
   // Wires.
   //----------------------------------------------------------------
-  reg [31 : 0] tmp_read_data;
-  reg          tmp_api_error;
   
 
   
@@ -84,66 +99,16 @@ module cttest(
     begin : reg_update
       if (!reset_n)
         begin
-          testop_a_reg   <= 32'h00000000;
-          testop_b_reg   <= 32'h00000000;
-          testresult_reg <= 32'h00000000;
+
         end
       else
         begin
-          if  (testop_a_we)
-            begin
-              testop_a_reg <= testop_a_new;
-            end
-          
-          if  (testop_b_we)
-            begin
-              testop_b_reg <= testop_b_new;
-            end
-          
-          if  (testresult_we)
-            begin
-              testresult_reg <= testresult_new;
-            end
+
         end
     end // reg_update
-
-
-  //----------------------------------------------------------------
-  // api_logic
-  //
-  // Main update logic.
-  //----------------------------------------------------------------
-  always @*
-    begin : api_logic
-      tmp_read_data = 32'h00000000;
-      testop_a_new  = 32'h00000000;
-      testop_a_we   = 0;
-      testop_b_new  = 32'h00000000;
-      testop_b_we   = 0;
-      
-      if (cs)
-        begin
-          if (we)
-            begin
-              // Read operations.
-            end
-          else
-            begin
-              // Read operations.
-
-              case (address)
-                ADDR0:
-                  begin
-                    tmp_read_data = VERSION;
-                  end
-            end
-        end
-    end // api_logic
   
 endmodule // cttest
 
-
-
 //======================================================================
-// EOF cttest
+// EOF coretest
 //======================================================================
