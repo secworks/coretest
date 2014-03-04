560704a41d47a4d951e3117eef6fb6e9b93b7ed9
Joachim Strömbergson <joachim@secworks.se>
2014-03-04 22:30:15 +0100

    Add missing reset of core address reg.

--- src/rtl/coretest.v
+++ src/rtl/coretest.v
@@ -261,11 +261,12 @@ module coretest(
 
           rx_buffer_ptr_reg   <= 4'h0;
           tx_buffer_ptr_reg   <= 4'h0;
-
+          
           core_reset_reg      <= 1;
           core_cs_reg         <= 0;
           core_we_reg         <= 0;
           core_error_reg      <= 0;
+          core_address_reg    <= 16'h0000;
           core_write_data_reg <= 32'h00000000;
           core_read_data_reg  <= 32'h00000000;
           
